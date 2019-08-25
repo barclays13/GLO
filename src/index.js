8bc2397587f05269b43a945fa702484002e7e508
Zubarev Sergey <barclays13@mail.ru>
2019-08-25 21:37:46 +0300

    первый комит в master

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('first commit');
+const money = 1 ;
+const income = 2;
+const deposit = 3;
+let mission = 4;
+let period = 5;
+let  addExpenses = 6; 
+
+alert('hello');
+
+console.log(money);
