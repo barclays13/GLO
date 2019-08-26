81aebe951802a6cd278d790b34dd645023abb802
barclays13@mail.ru <Sergey Zubarev>
2019-08-26 13:39:11 +0300

    3 домашка осная 1-6 задания

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-const money = 1 ;
-const income = 2;
-const deposit = 3;
+'use strict';
+
+let income = 2;
+
 let mission = 4;
 let period = 5;
-let  addExpenses = 6; 
 
-alert('hello');
 
-console.log(money);
+let money = +prompt('Ваш месячный дроход? ', 700); // Пользователь вводит месячный доход и мы его преобразуев в числ тип
+
+let  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Транспорт, питание, отдых').split(","); 
+console.log('Расходы= ', addExpenses);
+
+let deposit = confirm('Есть ли у вас депозит в банке?'); 
+
+console.log(typeof money);
+console.log(typeof addExpenses);
+console.log(typeof deposit);
+
+let costsMonth1 = prompt('Какие обязательные ежемесячные расходы у вас есть? ', 'Авто, питание, оплата счетов'); 
+let amountOfExpenses1 = +prompt('Во сколько это обойдется?', 400);
+let costsMonth2 = prompt('Какие обязательные ежемесячные расходы у вас есть? ', 'Шопинг, развелечения...');
+let amountOfExpenses2 = +prompt('Во сколько это обойдется?', 300);
+
+let budgetMonth = money - amountOfExpenses1 - amountOfExpenses2; //Доход за месяц
+console.log('Доход за месяц: ', budgetMonth); 
